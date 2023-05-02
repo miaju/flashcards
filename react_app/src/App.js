4aeae2a6684b74caaf33d7f55d4cd4d0f2adaa25
Mia Tanner <mia@indygecko.com>
2023-05-02 13:26:16 -0700

    add routes + other misc.

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,25 @@
+import { React } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+//import axios from "axios";
 
+import NavBar from "./components/Navbar";
 import CardItem from './components/CardItem';
+import Home from "./components/Home";
 
 function App() {
+
+  const testCard = {front: "front", back: "back"};
+
   return (
     <div className="App">
-    <CardItem front="front" back="back"/>
+    <BrowserRouter>
+    <NavBar></NavBar>
+    <Routes>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/test" element={<CardItem card={testCard}/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- react_app/src/components/CardItem.js
+++ react_app/src/components/CardItem.js
@@ -4,10 +4,11 @@ import Card from 'react-bootstrap/Card';
 export default function CardItem(props) {
   const [clicked, setClicked] = useState(false);
   const click = () => { setClicked(!clicked) }
+  const card = props.card;
   return (
     <Card onClick={click}>
     <Card.Body>
-    { clicked ? props.back : props.front }
+    { clicked ? card.back : card.front }
     </Card.Body>
       
     </Card>
