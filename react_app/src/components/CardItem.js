9b781564377233a7bb244bcadbaa5b85b19860c4
Mia Tanner <54921297+miaju@users.noreply.github.com>
2023-05-03 13:02:44 -0700

    Components/card item (#2)
    * add basic card + card spinning mechanic
    
    * make minor style edits

--- react_app/src/components/CardItem.js
+++ react_app/src/components/CardItem.js
@@ -1,16 +1,19 @@
 import { useState } from "react"
 import Card from 'react-bootstrap/Card';
 
+import "./CardItem.scss"
+
 export default function CardItem(props) {
   const [clicked, setClicked] = useState(false);
   const click = () => { setClicked(!clicked) }
   const card = props.card;
+  const className = clicked ? "back" : "front"
+
   return (
-    <Card onClick={click}>
+    <Card bg="dark" text="light" className={className} onClick={click}>
     <Card.Body>
     { clicked ? card.back : card.front }
     </Card.Body>
-      
     </Card>
   )
 };
