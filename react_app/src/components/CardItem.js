312ef3c9e16521d244a285b0ee3a517bee34c1b4
Mia Tanner <mia@indygecko.com>
2023-07-17 12:54:57 -0700

    make flashcard switch to front when going to next card

--- react_app/src/components/CardItem.js
+++ react_app/src/components/CardItem.js
@@ -1,18 +1,16 @@
-import { useState } from "react"
 import Card from 'react-bootstrap/Card';
 
 import "./CardItem.scss"
 
 export default function CardItem(props) {
-  const [clicked, setClicked] = useState(false);
-  const click = () => { setClicked(!clicked) }
+
   const card = props.card;
-  const className = clicked ? "back" : "front"
+  const className = props.clicked ? "back" : "front"
 
   return (
-    <Card bg="dark" text="light" className={className} onClick={click}>
+    <Card bg="dark" text="light" className={className} onClick={props.click}>
     <Card.Body>
-    { clicked ? card.back : card.front }
+    { props.clicked ? card.back : card.front }
     </Card.Body>
     </Card>
   )
