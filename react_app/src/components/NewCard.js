6ad74784a72f64e7a88daf1ee25a3c36eefbb983
Mia Tanner <mia@indygecko.com>
2023-07-18 16:32:30 -0700

    add error message for empty flashcard submit

--- react_app/src/components/NewCard.js
+++ react_app/src/components/NewCard.js
@@ -8,23 +8,30 @@ export default function NewCard(props) {
   const [front, setFront] = useState('');
   const [back, setBack] = useState('');
   const [clicked, setClicked] = useState(false);
+  const [showErr, setShowErr] = useState(false);
 
   const click = () => { setClicked(!clicked) }
 
   function submit() {
+    if (front === '' || back === '') {
+      return setShowErr(true);
+    }
     console.log(front, back);
   }
 
   return(
     <>
+      <div className="form-error">
+        {showErr && "Something went wrong. Please make sure that the inputs are non-empty and try again."}
+      </div>
       <Form>
         <Form.Group className="mb-3" controlId="newCard.front">
           <Form.Label>Flashcard front</Form.Label>
-          <Form.Control value={front} onChange={(event) => setFront(event.target.value)} as="textarea" rows={1} />
+          <Form.Control value={front} onChange={(event) => {setFront(event.target.value); setShowErr(false)}} as="textarea" rows={1} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="newCard.back">
           <Form.Label>Flashcard back</Form.Label>
-          <Form.Control value={back} onChange={(event) => setBack(event.target.value)} as="textarea" rows={1} />
+          <Form.Control value={back} onChange={(event) => {setBack(event.target.value); setShowErr(false)}} as="textarea" rows={1} />
         </Form.Group>
       </Form>
       <CardItem card={{front, back}} clicked={clicked} click={click}/>
