7bcde3c862012def8435c486bbae6e0aefbbe3ce
Mia Tanner <mia@indygecko.com>
2023-08-22 17:10:09 -0700

    make logout popup

--- react_app/src/components/Navbar.js
+++ react_app/src/components/Navbar.js
@@ -1,10 +1,11 @@
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import "./Navbar.scss";
 
-export default function NavBar() {
+export default function NavBar({ logout }) {
   return (
   <Navbar sticky="top" bg="light" expand="lg">
       <Container id="test">
@@ -18,8 +19,10 @@ export default function NavBar() {
             <Nav.Link href="/new">New</Nav.Link>
             <Nav.Link href="/cards">Cards</Nav.Link>
             <Nav.Link href="/profile">Profile</Nav.Link>
-            <Nav.Link href="/logout" >Logout</Nav.Link>
             <Nav.Link href="/login" >Login</Nav.Link>
+            <Button>
+              <Nav.Link href="" onClick={() => logout(true)}>Logout</Nav.Link>
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- react_app/src/components/User/Profile.js
+++ react_app/src/components/User/Profile.js
@@ -1,3 +1,27 @@
-export default function Profile(props) {
-  return;
-}
+import React from "react";
+import { Button } from "react-bootstrap";
+
+import "./Profile.scss";
+import Logout from "./Logout";
+
+export default function Profile({ user, logout, show }) {
+
+  return (
+    <div className="profile">
+      {show && <Logout show={show} logout={logout}/>}
+      <h1>Profile</h1>
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <div className="info">
+          <p>Name: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p><Button size="sm" variant="outline-secondary" href="/pets/view"> View My Flashcard Sets </Button></p>
+          <p><Button size="sm" variant="outline-secondary" href="/pets/new">Create New Flashcard Set </Button></p>
+          <p><Button size="lg" variant="danger" onClick={() => logout(true)}>Logout</Button></p>
+        </div>
+      </div>
+    </div>
+  
+  );
+};
+
