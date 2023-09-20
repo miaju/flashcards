d184f03b7f24a495dedb0bdc53bb9e6738aa276c
Mia Tanner <mia@indygecko.com>
2023-09-20 14:58:31 -0700

    make route to filter sets by keyword

--- django_api/api_app/urls.py
+++ django_api/api_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, SetViewSet, CardViewSet, SetsByUserView, CardsBySetsView
+from .views import UserViewSet, SetViewSet, CardViewSet, SetsByUserView, CardsBySetsView, SetsByKeywordView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -10,5 +10,7 @@ router.register(r'cards', CardViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('sets/user/<int:user_id>/', SetsByUserView.as_view(), name='user-sets'),
-    path('cards/sets/<int:set_id>/', CardsBySetsView.as_view(), name='set-cards')
+    path('cards/sets/<int:set_id>/', CardsBySetsView.as_view(), name='set-cards'),
+    path('sets/keyword/<str:keyword>/', SetsByKeywordView.as_view(), name='sets-by-keyword'),
+    
 ]
--- django_api/api_app/views.py
+++ django_api/api_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, generics
+from django.db.models import Q
 from .models import User, Set, Card
 from .serializers import UserSerializer, SetSerializer, CardSerializer
 
@@ -30,3 +31,10 @@ class CardsBySetsView(generics.ListAPIView):
     def get_queryset(self):
         set_id = self.kwargs['set_id']  # Get the set ID from the URL parameter
         return Card.objects.filter(set_id=set_id)
+
+
+class SetsByKeywordView(generics.ListAPIView):
+    serializer_class = SetSerializer
+    def get_queryset(self):
+        keyword = self.kwargs['keyword'].lower()
+        return Set.objects.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword))
