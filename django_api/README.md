cf144d3260a3b2ef774b52f847105fb54065be92
Mia Tanner <54921297+miaju@users.noreply.github.com>
2023-09-20 15:03:09 -0700

    create more routes in the api (#14)
    * create requirements.txt and readme
    
    * create get sets by user and get cards by set routes
    
    * make route to filter sets by keyword

---
+++ django_api/README.md
@@ -0,0 +1,24 @@
+# Api server
+
+## Getting Started
+
+In this folder, run the command ```pip install -r requirements.txt``` to install the dependencies.
+
+## Running the server
+
+Use the command ```python manage.py runserver``` to start the server.
+
+## The database
+
+### Seeding
+
+```bash
+python manage.py seeds 
+python mangage.py generate_fake_data
+```
+
+### Resetting the database
+
+```bash
+python manage.py flush
+```
--- django_api/api_app/views.py
+++ django_api/api_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from django.db.models import Q
 from .models import User, Set, Card
 from .serializers import UserSerializer, SetSerializer, CardSerializer
 
@@ -16,3 +17,24 @@ class SetViewSet(viewsets.ModelViewSet):
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+class SetsByUserView(generics.ListAPIView):
+    serializer_class = SetSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']  # Get the user ID from the URL parameter
+        return Set.objects.filter(user_id=user_id)
+
+class CardsBySetsView(generics.ListAPIView):
+    serializer_class = CardSerializer
+
+    def get_queryset(self):
+        set_id = self.kwargs['set_id']  # Get the set ID from the URL parameter
+        return Card.objects.filter(set_id=set_id)
+
+
+class SetsByKeywordView(generics.ListAPIView):
+    serializer_class = SetSerializer
+    def get_queryset(self):
+        keyword = self.kwargs['keyword'].lower()
+        return Set.objects.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword))
